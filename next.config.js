4ba640de7e405788d38db3e8fbbd6633c58361db
moehami <m.aljaaly@yahoo.co.uk>
2024-12-03 17:38:01 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,5 +6,12 @@ const nextConfig = {
   },
   images: { unoptimized: true },
 };
+typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  }
 
 module.exports = nextConfig;
